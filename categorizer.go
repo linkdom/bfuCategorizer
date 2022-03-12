85e9b63c34a63b44a041ba07845672d32acc7a0c
Dominik Link <vorbild94@gmail.com>
2022-03-12 14:24:51 +0100

    started to sanitize strings, prepared request

--- categorizer.go
+++ categorizer.go
@@ -6,6 +6,8 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"regexp"
+	"strings"
 )
 
 type Games struct {
@@ -17,6 +19,7 @@ type Game struct {
 	Publisher   string `json:"publisher"`
 	Link        string `json:"link"`
 	Description string `json:"description"`
+	Genre       string
 }
 
 func main() {
@@ -35,9 +38,45 @@ func main() {
 	json.Unmarshal(byteValue, &games)
 
 	for i := 0; i < len(games.Games); i++ {
-		fmt.Println("Name: " + games.Games[i].Name)
+
+		regex := regexp.MustCompile(`\[([^\[\]]*)\]`)
+		match := regex.FindAllStringSubmatch(games.Games[i].Name, -1)
+
+		if len(m) != 0 {
+
+			r := strings.Replace(games.Games[i].Name, "["+match[0][1]+"]", "", 10)
+
+			s := strings.Replace(r, " ", "-", 10)
+			s = strings.Replace(s, ".", "-dot-", 10)
+			s = strings.TrimPrefix(s, "-")
+			s = strings.TrimSuffix(s, "-")
+			fmt.Println("Name: " + strings.ToLower(s))
+
+		} else {
+			s := strings.Replace(games.Games[i].Name, " ", "-", 10)
+			s = strings.Replace(s, ".", "-dot-", 10)
+			fmt.Println("Name: " + strings.ToLower(s))
+		}
+
 		fmt.Println("Publisher: " + games.Games[i].Publisher)
 		fmt.Println("Link: " + games.Games[i].Link)
-		fmt.Println("Description: " + games.Games[i].Description)
+		fmt.Println("Description: " + games.Games[i].Description + "\n")
 	}
+
+	/*
+		resp, err := http.Get("https://www.igdb.com/games/"+s)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer resp.Body.Close()
+
+		body, err := ioutil.ReadAll(resp.Body)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+	*/
 }
